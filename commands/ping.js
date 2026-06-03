a1f9b5285d47178bfcd182963dedf701c309b5a6
RYK!LE <rekill0424@gmail.com>
2026-06-04 00:06:34 +0900

    ping.js を更新

--- commands/ping.js
+++ commands/ping.js
@@ -1,1 +1,11 @@
-か
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Botの応答確認"),
+
+  async execute(interaction) {
+    await interaction.reply("🏓 Pong!");
+  },
+};
