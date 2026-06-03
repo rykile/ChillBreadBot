9d023d0fc4849336347668ce0e87804cf906405a
RYK!LE <rekill0424@gmail.com>
2026-06-04 00:27:27 +0900

    addbot.js を更新

--- commands/addbot.js
+++ commands/addbot.js
@@ -1,41 +1,41 @@
 const {
-SlashCommandBuilder,
-ActionRowBuilder,
-ButtonBuilder,
-ButtonStyle,
-} = require(“discord.js”);
+  SlashCommandBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+} = require("discord.js");
 
-const config = require(”../config”);
+const config = require("../config");
 
 function generateInviteUrl() {
-return https://discord.com/oauth2/authorize?client_id=${process.env.CLIENT_ID}&permissions=${config.permissions}&scope=bot%20applications.commands;
+  return `https://discord.com/oauth2/authorize?client_id=${process.env.CLIENT_ID}&permissions=${config.permissions}&scope=bot%20applications.commands`;
 }
 
 module.exports = {
-data: new SlashCommandBuilder()
-.setName(“addbot”)
-.setDescription(“Botを導入します（招待チャンネル専用）”),
+  data: new SlashCommandBuilder()
+    .setName("addbot")
+    .setDescription("Botを導入します"),
 
-async execute(interaction) {
-if (interaction.channelId !== config.INVITE_CHANNEL_ID) {
-return interaction.reply({
-content: “このコマンドは招待チャンネルでのみ使用できます。”,
-ephemeral: true,
-});
-}
+  async execute(interaction) {
+    if (interaction.channelId !== config.INVITE_CHANNEL_ID) {
+      return interaction.reply({
+        content: "このコマンドは招待チャンネルでのみ使用できます。",
+        ephemeral: true,
+      });
+    }
 
-const url = generateInviteUrl();
-const row = new ActionRowBuilder().addComponents(
-  new ButtonBuilder()
-    .setLabel("🤖 Botを追加")
-    .setStyle(ButtonStyle.Link)
-    .setURL(url)
-);
-await interaction.reply({
-  content: "ここからワンタップで導入できます。",
-  components: [row],
-  ephemeral: true,
-});
+    const row = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setLabel("🤖 Botを追加")
+        .setStyle(ButtonStyle.Link)
+        .setURL(generateInviteUrl())
+    );
 
-},
+    await interaction.reply({
+      content: "こちらからBotを導入できます。",
+      components: [row],
+      ephemeral: true,
+    });
+  },
+};
 };
