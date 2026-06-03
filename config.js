0b0b3fe2986549e4937163c1802d32070598d183
RYK!LE <rekill0424@gmail.com>
2026-06-04 00:26:16 +0900

    config.js を更新

--- config.js
+++ config.js
@@ -1,29 +1,12 @@
-{\rtf1\ansi\ansicpg932\cocoartf2870
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+module.exports = {
+  permissions: "8",
 
-\f0\fs24 \cf0 module.exports = \{\
-  // \uc0\u25307 \u24453 \u23554 \u29992 \u12481 \u12515 \u12531 \u12493 \u12523 \
-  INVITE_CHANNEL_ID: "1506605319357464659",\
-\
-  // Bot\uc0\u25805 \u20316 \u35377 \u21487 \u12481 \u12515 \u12531 \u12493 \u12523 \u65288 \u35079 \u25968 \u65289 \
-  ALLOWED_CHANNELS: [\
-    "1506605319357464659",\
-    "1506605435082375219",\
-    "1506605496730128565",\
-    "1506605706093138123",\
-    "1506605768286277712",\
-    "1506605819439874058",\
-  ],\
-\
-  // \uc0\u12514 \u12540 \u12489 \
-  // soft = \uc0\u28961 \u35222 \u12398 \u12415 \
-  // hard = \uc0\u21066 \u38500 \u65291 \u35686 \u21578 \
-  ANTISPAM_MODE: "hard",\
-\
-  // \uc0\u25307 \u24453 \u27177 \u38480 \
-  permissions: "8",\
-\};}
+  INVITE_CHANNEL_IDS: [
+    "1506605319357464659",
+    "1506605435082375219",
+    "1506605496730128565",
+    "1506605706093138123",
+    "1506605768286277712",
+    "1506605819439874058",
+  ],
+};
